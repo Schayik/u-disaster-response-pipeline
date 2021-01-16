6e64b982a0d55d7d86ecaca3acf5a1253061c55b
Pim van Schayik <pimvschayik@live.nl>
2021-01-16 11:16:30 +0000

    adjusted train_classifier.py to setup ML pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,71 @@
 import sys
+import re
+import pickle
+import pandas as pd
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['stopwords', 'punkt', 'wordnet'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('messages', engine)
+    X = df['message']
+    category_names = [x for x in df.columns.values if x not in ['id', 'message', 'original', 'genre']]
+    Y = df[category_names]
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    normalized = re.sub(r"[^a-zA-Z0-9]", " ", text).lower().strip()
+    tokenized = word_tokenize(normalized)
+    without_stop_words = [
+        w for w in tokenized if w not in stopwords.words('english')
+    ]
+
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in without_stop_words:
+        clean_tok = lemmatizer.lemmatize(tok)
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    model = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+    ])
+
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    Y_pred_df = pd.DataFrame(Y_pred, columns=category_names)
+
+    for column in Y_test.columns:
+        print(column)
+        print(classification_report(Y_test[column], Y_pred_df[column]))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
